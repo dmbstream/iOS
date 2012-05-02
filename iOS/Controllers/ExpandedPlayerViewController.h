f3c62304151be09414236b0293953faaf5ee6bd9
Jim Geurts <jim@biacreations.com>
2012-05-01 22:52:46 -0500

    Move ExpandedPlayer into storyboard. Wire up the expanded player show/hide logic from the main menu

---
+++ iOS/Controllers/ExpandedPlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExpandedPlayerViewController.h
+//  iOS
+//
+//  Created by James Geurts on 5/1/12.
+//  Copyright (c) 2012 Bia Creations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpandedPlayerViewController : UIViewController
+
+@end
